5af2b633750517c00587da93b7b3412c71823974
jnicolay <jens.nicolay@gmail.com>
2017-08-04 15:16:05 +0200

    cleanup phase 2

--- generated.js
+++
@@ -1,7 +0,0 @@
-{
-    i = I();
-    I = function (p0) { return join(null, boolean); };
-    I.x = null;
-    I.f = function (p0, p1) { return join(string, boolean); };
-}
-//# sourceMappingURL=generated.js.map
