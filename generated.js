08ad3645734b0e675ba7e3b217d6a54a232511b7
jnicolay <jens.nicolay@gmail.com>
2017-08-04 15:14:27 +0200

    cleanup phase 1

---
+++ generated.js
@@ -0,0 +1,7 @@
+{
+    i = I();
+    I = function (p0) { return join(null, boolean); };
+    I.x = null;
+    I.f = function (p0, p1) { return join(string, boolean); };
+}
+//# sourceMappingURL=generated.js.map
---
+++ todeffiletest.js
@@ -0,0 +1,9 @@
+function f(x, y) {
+    if (typeof x === "string") {
+        return true;
+    }
+    return 123;
+}
+// f(3);
+f("xxx", false);
+//# sourceMappingURL=todeffiletest.js.map
